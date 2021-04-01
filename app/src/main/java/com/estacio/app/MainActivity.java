d4ce1bf00f004ba59f1b4064bf9f0df7912c7921
Dion Laranjeira <dionribeiro.rr@gmail.com>
2021-04-01 19:36:32 -0400

    Aula 01/04/20121
    Adição de componentes do Google Material Design

--- app/src/main/java/com/estacio/app/MainActivity.java
+++ app/src/main/java/com/estacio/app/MainActivity.java
@@ -8,30 +8,43 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText editTextLogin;
-    EditText editTextSenha;
-    Button buttonLogar;
-    Button buttonCadastrar;
+//    EditText editTextLogin;
+    private TextInputLayout textInputLayoutLogin;
+//    EditText editTextSenha;
+    private TextInputLayout textInputLayoutSenha;
+    private Button buttonLogar;
+    private Button buttonCadastrar;
+    private String loginPadrao = "dion";
+    private String senhaPadrao = "123";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextLogin = findViewById(R.id.editTextLogin);
-        editTextSenha = findViewById(R.id.editTextSenha);
+        textInputLayoutLogin = findViewById(R.id.textFieldLogin);
+        textInputLayoutSenha = findViewById(R.id.TextInputLayoutSenha);
         buttonLogar = findViewById(R.id.buttonLogar);
 
         buttonLogar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String login = editTextLogin.getText().toString();
-                String senha = editTextSenha.getText().toString();
-                String mensagem = "Seu login é " + login +"\nSua senha é "+ senha;
-
-                Toast.makeText(MainActivity.this, mensagem, Toast.LENGTH_SHORT).show();
+                String login = textInputLayoutLogin.getEditText().getText().toString();
+                String senha = textInputLayoutSenha.getEditText().getText().toString();
+                if(login.equals(loginPadrao) && senha.equals(senhaPadrao)){
+                    String mensagem = "LOGADO";
+                    Toast.makeText(MainActivity.this, mensagem, Toast.LENGTH_SHORT).show();
+                    textInputLayoutSenha.setError("");
+                }else{
+                    String mensagem = "Não logado";
+                    Toast.makeText(MainActivity.this, mensagem, Toast.LENGTH_SHORT).show();
+                    textInputLayoutSenha.setError("Login ou senha inválido");
+                }
             }
         });
 
