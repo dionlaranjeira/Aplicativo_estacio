e18995ad93ac993d2855a6fa490e34e032f1cd04
DION RIBEIRO LARANJEIRA <dionribeiro.rr@gmail.com>
2021-04-29 20:14:06 -0400

    Aula dia 29-04-2021
    Adicao do Firebase Auth

--- app/src/main/java/com/estacio/app/CadastroActivity.java
+++ app/src/main/java/com/estacio/app/CadastroActivity.java
@@ -1,19 +1,26 @@
 package com.estacio.app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.estacio.app.model.Usuario;
 import com.estacio.app.utilities.TextMask;
 import com.estacio.app.utilities.ValidaCampos;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class CadastroActivity extends AppCompatActivity {
 
+    private FirebaseAuth autenticacao;
     TextInputLayout textInputLayoutNome;
     TextInputLayout textInputLayoutDataNasc;
     TextInputLayout textInputLayoutCelular;
@@ -26,6 +33,43 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        //Pegar a instancia do FireBaseAuth
+
+        autenticacao = FirebaseAuth.getInstance();
+
+        //Cria o usuario
+        autenticacao
+                .createUserWithEmailAndPassword("marcelo@gmail.com","123456").
+                addOnCompleteListener(CadastroActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(CadastroActivity.this, "Login criado com sucesso", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(CadastroActivity.this, "Falha ao criar login", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        // deslogar usuario.
+        autenticacao.signOut();
+
+        // Logar usuario ja cadastrado
+
+        autenticacao.signInWithEmailAndPassword("dionribeiro.rr@gmail.com","123456").
+                addOnCompleteListener(CadastroActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(CadastroActivity.this, "Usuario logado com sucesso", Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(CadastroActivity.this, "Login ou senha invalido", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+
 
         inicializaComponentes();
         validaCamposEMascaras();
